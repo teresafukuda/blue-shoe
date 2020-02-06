0854a361c72d013e74982a3e085ac241088afd73
patersonandrew <andrew.r.paters@gmail.com>
2020-02-05 17:35:23 -0800

    Made graph of mass change vs tread depth change.  Regressed between two, p value is absurdly high.

--- polished_graphs_and_stats.R
+++ polished_graphs_and_stats.R
@@ -22,11 +22,14 @@ library(plm)# regression analysis package
 grams_steps_lm <- lm(mass_change ~ steps, data = full_data_joined)
 summary(grams_steps_lm)
 
+# P value is less than .01. Multiple R squared is .06152
+
 # Adding trendline to graph #
 
 grams_steps_scatter_lm <- ggplot(full_data_joined, aes(x=steps, y=mass_change))+
   geom_point()+
   theme_bw()+
+  labs(x = "Steps Taken", y = "Mass Change (g)")+
   geom_smooth(method = "lm", color = "indianred1") # add se = FALSE to remove error bar
 
 grams_steps_scatter_lm
@@ -34,5 +37,19 @@ grams_steps_scatter_lm
 #### Questions for group ####  
 # should we add R squared and slope to the graph?  
 # Should we remove error bar?
+# Should we remove high outliers?
 
+
+################## Graph of mass change vs. tread depth lost #################
+
+tread_mass_joined <- full_join(tread_joined_details, full_data_joined) %>% 
+  drop_na()
+
+mass_vs_tread <- ggplot(tread_mass_joined, aes(x=avg_depth_change, y=mass_change))+
+  geom_point()+
+  theme_bw()
+
+mass_vs_tread
+
+# Ran a regression between mass lost and tread depth change, P value is absurdly high (.971). 
 
