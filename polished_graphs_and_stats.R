5ed690f876d077986a65f5e4cea57b07ba1a25d3
patersonandrew <andrew.r.paters@gmail.com>
2020-02-05 19:58:13 -0800

    Ran ANOVA and Tukey's HSD for mass and tread depth change versus our vairables.  Only tread depth vs. geometry had a significant difference in means, and that was only between lifestyle and running.

--- polished_graphs_and_stats.R
+++ polished_graphs_and_stats.R
@@ -57,10 +57,13 @@ mass_vs_tread
 
 ### Normalize tread depth by steps taken ###
 
-tread_calculations <- tread_mass_joined %>% 
+tread <- tread_mass_joined %>% 
   mutate("mm_per_mile"= avg_depth_change/miles) %>% 
   mutate("weight_kg"= weight*0.453592) %>% 
-  mutate("mm_per_miles_per_kg"=mm_per_mile/weight_kg)
+  mutate("mm_per_miles_per_kg"=mm_per_mile/weight_kg) 
+
+tread_calculations <- tread %>% ### Remove rows that prevent regression
+  filter(steps != 0) 
 
 # Tread depth vs rubber type
 
@@ -88,3 +91,63 @@ tread_change_vs_geometry <- ggplot(tread_calculations, aes(x=geometry, y=mm_per_
   theme(axis.text.x = element_text(angle = 75, hjust=1))
 
 tread_change_vs_geometry
+
+### Tukey's HSD for tread depth vs. our variables ###
+
+# First, ANOVA for each variable/relationship
+
+# Rubber Type
+tread_rubber_lm <- lm(mm_per_mile ~ rubber_type, data = tread_calculations)
+tread_rubber_av <- aov(tread_rubber_lm)
+summary(tread_rubber_av)
+
+# P-value is 0.6, so no significant difference in means. No Tukey's HSD
+
+# Hardness
+tread_hardness_lm <- lm(mm_per_mile ~ hardness, data = tread_calculations)
+tread_hardness_av <- aov(tread_hardness_lm)
+summary(tread_hardness_av)
+
+# P-value is 0.847, so no significant difference in means. No Tukey's HSD
+
+# Geometry
+tread_geometry_lm <- lm(mm_per_mile ~ geometry, data = tread_calculations)
+tread_geometry_av <- aov(tread_geometry_lm)
+summary(tread_geometry_av)
+
+# P-value is 0.0383, so a significant difference in means. Tukey's HSD:
+
+tread_geometry_tukey <- TukeyHSD(tread_geometry_av)
+tread_geometry_tukey
+
+# Only significant difference is between running and lifestyle. 
+
+### Tukey's HSD for mass change vs our variables ###
+
+# First, remove rows that prevent regression from occuring (g_per_steps = 0, because this gives -Inf for g_per_milesteps)
+
+step_calc_for_Tukey <- step_calculations %>% 
+  filter(steps != 0)
+
+# Then, ANOVA for each variable/relationship
+
+# Rubber Type
+mass_rubber_lm <- lm(g_per_milesteps ~ rubber_type, data = step_calc_for_Tukey)
+mass_rubber_av <- aov(mass_rubber_lm)
+summary(mass_rubber_av)
+
+# P-value is 0.138, so no significant difference in means. No Tukey's HSD
+
+# Hardness
+mass_hardness_lm <- lm(g_per_milesteps ~ hardness, data = step_calc_for_Tukey)
+mass_hardness_av <- aov(mass_hardness_lm)
+summary(mass_hardness_av)
+
+# P-value is 0.547, so no significant difference in means. No Tukey's HSD
+
+# Geometry
+mass_geometry_lm <- lm(g_per_milesteps ~ geometry, data = step_calc_for_Tukey)
+mass_geometry_av <- aov(mass_geometry_lm)
+summary(mass_geometry_av)
+
+# P-value is 0.983, so no significant difference in means. No Tukey's HSD
